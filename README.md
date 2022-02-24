81576f38258ad124172b6ba2a5427efe23db6385
ilyesedina <44023617+ilyesedina@users.noreply.github.com>
2022-02-24 21:33:48 +0100

    dark-mode-toggle

---
+++ README.md
@@ -0,0 +1,7 @@
+## javascript-sudoku
+
+    Make Sudoku Game With HTML CSS JavaScript
+
+### Link to the side
+
+    https://ilyesedina.github.io/Sudoku-app/
---
+++ root/scripts/app.js
@@ -0,0 +1,7 @@
+document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDarkMode = document.body.classList.contains('dark');
+    localStorage.setItem('darkmode', isDarkMode);
+    // chang mobile status bar color
+    document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff');
+});
