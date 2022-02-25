f0d26542c2962ed02ec04e60539ba4faddca7fac
ilyesedina <44023617+ilyesedina@users.noreply.github.com>
2022-02-25 01:09:59 +0100

    start screen

--- root/scripts/app.js
+++ root/scripts/app.js
@@ -6,6 +6,61 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff');
 });
 
+// screens
+const start_screen = document.querySelector('#start-screen');
+const game_screen = document.querySelector('#game-screen');
+const pause_screen = document.querySelector('#pause-screen');
+const result_screen = document.querySelector('#result-screen');
+// ----------
+const cells = document.querySelectorAll('.main-grid-cell');
+
+const name_input = document.querySelector('#input-name');
+
+const number_inputs = document.querySelectorAll('.number');
+
+const player_name = document.querySelector('#player-name');
+const game_level = document.querySelector('#game-level');
+const game_time = document.querySelector('#game-time');
+
+const result_time = document.querySelector('#result-time');
+
+let level_index = 0;
+let level = CONSTANT.LEVEL[level_index];
+
+let timer = null;
+let pause = false;
+let seconds = 0;
+
+let su = undefined;
+let su_answer = undefined;
+
+let selected_cell = -1;
+
+// --------
+
+const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
+
+// add button event
+document.querySelector('#btn-level').addEventListener('click', (e) => {
+    level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+    level = CONSTANT.LEVEL[level_index];
+    e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+});
+
+document.querySelector('#btn-play').addEventListener('click', () => {
+    if (name_input.value.trim().length > 0) {
+        initSudoku();
+        startGame();
+    } else {
+        name_input.classList.add('input-err');
+        setTimeout(() => {
+            name_input.classList.remove('input-err');
+            name_input.focus();
+        }, 500);
+    }
+});
+// -------------
+
 const init = () => {
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
     document.body.classList.add(darkmode ? 'dark' : 'light');
